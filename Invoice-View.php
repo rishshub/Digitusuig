e701b1eef6b22185804fdfa02f9f094bd6a6f3ea
PUNEETRKRAJPUT <128739405+PUNEETRKRAJPUT@users.noreply.github.com>
2023-11-01 14:13:16 +0530

    invoice error handled

--- Invoice-View.php
+++ Invoice-View.php
@@ -1,11 +1,12 @@
 <?php
-include("/config/config.php");
-/*
+session_start();
+include("config/config.php");
+
 $id=$_POST['view-invoice'];
 
 $sql="SELECT * from invoice WHERE ID='$id'";
 $result=mysqli_query($conn,$sql);
-$row=mysqli_fetch_array($result);*/
+$row=mysqli_fetch_array($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,47 +49,40 @@ $row=mysqli_fetch_array($result);*/
         <h1 class="font-semibold text-center text-2xl my-5">Download Invoice</h1>
         
         <div class="bg-white border rounded-lg shadow-lg px-6 py-8 max-w-md mx-auto mt-8">
-    <h1 class="font-bold text-2xl my-4 text-center text-blue-600">KRP Services</h1>
+    <h1 class="font-bold text-2xl my-4 text-center text-blue-600">GLA Services</h1>
     <hr class="mb-2">
     <div class="flex justify-between mb-6">
         <h1 class="text-lg font-bold">Invoice</h1>
         <div class="text-gray-700">
-            <div>Date: 01/05/2023</div>
-            <div>Invoice #: INV12345</div>
+            <div>Date: <?php echo $row['Date'];?></div>
+            <div>Invoice #: INV<?php echo $row['ID'];?></div>
         </div>
     </div>
     <div class="mb-8">
         <h2 class="text-lg font-bold mb-4">Bill To:</h2>
-        <div class="text-gray-700 mb-2">John Doe</div>
-        <div class="text-gray-700 mb-2">123 Main St.</div>
-        <div class="text-gray-700 mb-2">Anytown, USA 12345</div>
-        <div class="text-gray-700">johndoe@example.com</div>
+        <div class="text-gray-700 mb-2"><?php echo $row['Name'];?></div>
+        <div class="text-gray-700 mb-2"><?php echo $row['Address'];?></div>
+        
+        <div class="text-gray-700"><?php echo $row['Email'];?></div>
     </div>
     <table class="w-full mb-8">
         <thead>
             <tr>
                 <th class="text-left font-bold text-gray-700">Description</th>
-                <th class="text-right font-bold text-gray-700">Amount</th>
+                <th class="text-right font-bold text-gray-700"><?php echo $row['Amount'];?></th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td class="text-left text-gray-700">Product 1</td>
-                <td class="text-right text-gray-700">$100.00</td>
+                <td class="text-left text-gray-700"><?php echo $row['Product'];?></td>
+                <td class="text-right text-gray-700">$<?php echo $row['Amount'];?></td>
             </tr>
-            <tr>
-                <td class="text-left text-gray-700">Product 2</td>
-                <td class="text-right text-gray-700">$50.00</td>
-            </tr>
-            <tr>
-                <td class="text-left text-gray-700">Product 3</td>
-                <td class="text-right text-gray-700">$75.00</td>
-            </tr>
+            
         </tbody>
         <tfoot>
             <tr>
                 <td class="text-left font-bold text-gray-700">Total</td>
-                <td class="text-right font-bold text-gray-700">$225.00</td>
+                <td class="text-right font-bold text-gray-700">$<?php echo $row['Amount'];?></td>
             </tr>
         </tfoot>
     </table>
